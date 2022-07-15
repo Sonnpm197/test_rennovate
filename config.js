cac3d07b7608d647e900633e699cb2f396052e88
Sonnpm197 <sonnpm197@gmail.com>
2022-07-15 14:47:31 +0700

    a

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  endpoint: 'https://api.github.com/',
+  token: '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',
+  platform: 'github',
+  logLevel: 'debug',
+  onboardingConfig: {
+    extends: ['config:base'],
+  },
+  repositories: ['Sonnpm197/test_rennovate'],
+  renovateFork: true,
+  gitAuthor: "Sonnpm197 <sonnpm197@gmail.com>",
+  username: "Sonnpm197",
+  onboarding: false,
+  printConfig: true,
+  requireConfig: false,
+};
